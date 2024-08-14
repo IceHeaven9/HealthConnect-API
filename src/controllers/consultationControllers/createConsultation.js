782d29e8cf04e7b0b3ab2b7390eba7fb5ab75558
Cristhian Medrano <cristhian_devfs@outlook.com>
2024-08-14 22:28:02 +0200

    controller

--- src/controllers/consultationControllers/createConsultation.js
+++ src/controllers/consultationControllers/createConsultation.js
@@ -21,18 +21,14 @@ export const createConsultationController = async (req, res) => {
 		return res.status(400).json({ mensaje: "Faltan datos necesarios" });
 	}
 
-	try {
-		const idConsultation = await crearCita({
-			title,
-			description,
-			severity,
-			specialityid,
-			patientid,
-			doctorid,
-			date,
-		});
-		res.status(201).json({ mensaje: "Cita creada", id: idConsultation });
-	} catch (error) {
-		res.status(500).json({ mensaje: "Error al crear la cita", error });
-	}
+	const idConsultation = await crearCita({
+		title,
+		description,
+		severity,
+		specialityid,
+		patientid,
+		doctorid,
+		date,
+	});
+	res.status(201).json({ mensaje: "Cita creada", id: idConsultation });
 };
