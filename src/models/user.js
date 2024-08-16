80876d00d80c2b17f9c41a0e95aefba84f4f3498
Cristhian Medrano <cristhian_devfs@outlook.com>
2024-08-16 19:02:35 +0200

    changes

--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,21 @@
-// models/User.js
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+import Joi from "joi";
+import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    resetPasswordToken: String,
-    resetPasswordExpires: Date,
-});
-
-// Método para hashear la contraseña antes de guardar
-userSchema.pre('save', async function(next) {
-    if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
-    next();
-});
-
-// Método para comparar contraseñas proporcionadas con la contraseña hasheada almacenada en la base de datos
-userSchema.methods.comparePassword = function(password) {
-    return bcrypt.compare(password, this.password);
+const userSchema = {
+	email: Joi.string().email().required(),
+	password: Joi.string().required(),
+	resetPasswordToken: Joi.string().optional(),
+	resetPasswordExpires: Joi.date().optional(),
 };
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+// Método para hashear la contraseña antes de guardar
+const hashPassword = async (user) => {
+	if (user.password) {
+		user.password = await bcrypt.hash(user.password, 10);
+	}
+};
+
+// Método para comparar contraseñas proporcionadas con la contraseña hasheada almacenada en la base de datos
+const comparePassword = (password, hashedPassword) => {
+	return bcrypt.compare(password, hashedPassword);
+};
