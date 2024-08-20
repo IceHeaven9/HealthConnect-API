e193a141745f31224303b8a372e7c95789110163
Cristhian Medrano <cristhian_devfs@outlook.com>
2024-08-20 18:12:26 +0200

    endpointrespuestaDeConsult

--- src/database/structure/initDb.js
+++ src/database/structure/initDb.js
@@ -1,30 +1,30 @@
 import {
-  errorLog,
-  infoLog,
-  succesLog,
-  warningLog,
+	errorLog,
+	infoLog,
+	succesLog,
+	warningLog,
 } from "../../utils/logger.js";
 import { Db } from "./db.js";
 
 async function initDB() {
-  try {
-    warningLog("Eliminando base de datos si existe");
-    await Db.query("DROP DATABASE IF EXISTS citas_medicas");
+	try {
+		warningLog("Eliminando base de datos si existe");
+		await Db.query("DROP DATABASE IF EXISTS citas_medicas");
 
-    await Db.query("CREATE DATABASE citas_medicas");
-    succesLog("Base de datos citas_medicas creada.");
+		await Db.query("CREATE DATABASE citas_medicas");
+		succesLog("Base de datos citas_medicas creada.");
 
-    await Db.query("USE citas_medicas");
-    infoLog("DB en uso: citas_medicas");
+		await Db.query("USE citas_medicas");
+		infoLog("DB en uso: citas_medicas");
 
-    warningLog("Eliminando tablas si existen");
-    await Db.query(
-      "DROP TABLE IF EXISTS ratings, responses, consultations, users, specialities"
-    );
+		warningLog("Eliminando tablas si existen");
+		await Db.query(
+			"DROP TABLE IF EXISTS ratings, responses, consultations, users, specialities"
+		);
 
-    infoLog("Creando tablas...");
+		infoLog("Creando tablas...");
 
-    await Db.query(`
+		await Db.query(`
             CREATE TABLE specialities (
                 id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
                 name VARCHAR(255) NOT NULL,
@@ -32,9 +32,9 @@ async function initDB() {
             )
         `);
 
-    succesLog("Tabla Specialities creada.");
+		succesLog("Tabla Specialities creada.");
 
-    await Db.query(`
+		await Db.query(`
             CREATE TABLE users (
                 id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
                 firstName VARCHAR(50) NOT NULL,
@@ -47,7 +47,6 @@ async function initDB() {
                 avatar VARCHAR(255) DEFAULT NULL,
                 specialityId INT DEFAULT NULL,
                 experience INT DEFAULT NULL,
-                mediaRating FLOAT DEFAULT NULL,
                 codigoMedico INT DEFAULT NULL,
                 validationCode INT DEFAULT NULL,
                 createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -55,12 +54,12 @@ async function initDB() {
             )
         `);
 
-    succesLog("Tabla Users creada.");
+		succesLog("Tabla Users creada.");
 
-    await Db.query(`
+		await Db.query(`
             CREATE TABLE consultations (
                 id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                date DATETIME DEFAULT CURRENT_TIMESTAMP,
+                date DATETIME NOT NULL,
                 title VARCHAR(255) NOT NULL,
                 description VARCHAR(255) NOT NULL,
                 specialityId INT NOT NULL,
@@ -75,44 +74,27 @@ async function initDB() {
             )
         `);
 
-    succesLog("Tabla consultations creada.");
+		succesLog("Tabla consultations creada.");
 
-    await Db.query(`
+		await Db.query(`
             CREATE TABLE responses (
                 id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                content TEXT NOT NULL,
+                content VARCHAR(5000) NOT NULL,
                 consultationId INT NOT NULL,
-                doctorId INT NOT NULL,
                 rating INT DEFAULT NULL,
-                date DATETIME DEFAULT CURRENT_TIMESTAMP,
                 createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
                 FOREIGN KEY (consultationId) REFERENCES consultations(id),
-                FOREIGN KEY (doctorId) REFERENCES users(id)
             )
         `);
 
-    succesLog("Tabla responses creada.");
+		succesLog("Tabla responses creada.");
 
-    await Db.query(`
-            CREATE TABLE ratings (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            userId INT NOT NULL,
-            consultationId INT NOT NULL,
-            rating INT NOT NULL,
-            createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (userId) REFERENCES users(id),
-            FOREIGN KEY (consultationId) REFERENCES consultations(id)
-        )
-            `);
-
-    succesLog("Tabla ratings creada.");
-
-    infoLog("¡Todas las tablas creadas exitosamente!");
-    process.exit(0);
-  } catch (error) {
-    errorLog("Error al crear las tablas:", error);
-    process.exit(1);
-  }
+		infoLog("¡Todas las tablas creadas exitosamente!");
+		process.exit(0);
+	} catch (error) {
+		errorLog("Error al crear las tablas:", error);
+		process.exit(1);
+	}
 }
 
 initDB();
