c1709270ee6acf7e38392c12efc16a95c88e5fe3
Cristhian Medrano <cristhian_devfs@outlook.com>
2024-08-21 23:39:53 +0200

    testing

--- src/database/consultation.js
+++ src/database/consultation.js
@@ -3,28 +3,28 @@ import { Db } from "./structure/db.js";
 
 // Funcion para crear una consulta
 export const createConsultation = async ({
-  title,
-  description,
-  severity,
-  specialityid,
-  id,
-  doctorid,
-  date,
+	title,
+	description,
+	severity,
+	specialityid,
+	id,
+	doctorid,
+	date,
 }) => {
-  const [result] = await Db.query(
-    `INSERT INTO consultations (title, description, severity, specialityid, patientId, doctorid, date) VALUES (?, ?, ?, ?, ?, ?,?)`,
-    [title, description, severity, specialityid, id, doctorid, date]
-  );
-  return result.insertId;
+	const [result] = await Db.query(
+		`INSERT INTO consultations (title, description, severity, specialityid, patientId, doctorid, date) VALUES (?, ?, ?, ?, ?, ?,?)`,
+		[title, description, severity, specialityid, id, doctorid, date]
+	);
+	return result.insertId;
 };
 
 // Función para obtener los datos de una consulta por id de consulta y solo si coincide con el id del paciente
 export const getConsultationDetails = async (req, res) => {
-  const userId = req.currentUser.id;
-  const consultationId = req.params.id;
+	const userId = req.currentUser.id;
+	const consultationId = req.params.id;
 
-  const [rows] = await Db.query(
-    `
+	const [rows] = await Db.query(
+		`
             SELECT 
                 c.title,
                 CONCAT(d.firstName, ' ', d.lastName) AS doctor,
@@ -49,23 +49,23 @@ export const getConsultationDetails = async (req, res) => {
             GROUP BY 
                 c.id;
         `,
-    [consultationId, userId]
-  );
+		[consultationId, userId]
+	);
 
-  if (rows.length === 0) {
-    throw generateErrors(404, "SERVER_ERROR", "Consulta no encontrada");
-  }
+	if (rows.length === 0) {
+		throw generateErrors(404, "SERVER_ERROR", "Consulta no encontrada");
+	}
 
-  res.status(200).json(rows[0]);
+	res.status(200).json(rows[0]);
 };
 
 // Función para obtener una consulta por el id de la consulta y solo si coincide con la especialidad del doctor
 export const getConsultationsById_BySpecialityId = async (
-  userId,
-  specialityId
+	userId,
+	specialityId
 ) => {
-  const [rows] = await Db.query(
-    `
+	const [rows] = await Db.query(
+		`
               SELECT 
                   c.title,
                   CONCAT(d.firstName, ' ', d.lastName) AS doctor,
@@ -98,19 +98,19 @@ export const getConsultationsById_BySpecialityId = async (
                   p.firstName,
                   p.lastName;
           `,
-    [userId, specialityId]
-  );
-  return rows;
+		[userId, specialityId]
+	);
+	return rows;
 };
 
 // Funcion para obtener todas las consultas de un paciente
 export const getConsultations = async (req, res) => {
-  const { title, speciality, severity, orderBy } = req.query;
+	const { title, speciality, severity, orderBy } = req.query;
 
-  const patientId = req.currentUser.id;
+	const patientId = req.currentUser.id;
 
-  const [rows] = await Db.query(
-    `
+	const [rows] = await Db.query(
+		`
             SELECT 
                 c.title,
                 CONCAT(d.firstName, ' ', d.lastName) AS doctor,
@@ -139,22 +139,22 @@ export const getConsultations = async (req, res) => {
             ORDER BY 
                 ${orderBy || "c.date DESC"};
         `,
-    [
-      `%${title || ""}%`,
-      `%${speciality || ""}%`,
-      `%${severity || ""}%`,
-      patientId,
-    ]
-  );
+		[
+			`%${title || ""}%`,
+			`%${speciality || ""}%`,
+			`%${severity || ""}%`,
+			patientId,
+		]
+	);
 
-  res.status(200).json(rows);
+	res.status(200).json(rows);
 };
 
 // Funcion para obtener una consulta por id
 // Se utiliza para obtener una consulta sin condiciones
 export const getConsultationById = async (id) => {
-  const [consultation] = await Db.query(
-    `
+	const [consultation] = await Db.query(
+		`
         SELECT 
             c.title,
             CONCAT(d.firstName, ' ', d.lastName) AS doctor,
@@ -174,38 +174,41 @@ export const getConsultationById = async (id) => {
         WHERE 
             c.id = ?
         `,
-    [id]
-  );
-  return consultation;
+		[id]
+	);
+	return consultation;
 };
 
 // Funcion para obtener todas las consultas por la id de su especialidad
 export const getConsultationsBySpecialityId = async (specialityId) => {
-  const [consultations] = await Db.query(
-    `SELECT 
+	const [consultations] = await Db.query(
+		`SELECT 
         c.id,
-        c.descripcion,
+        c.description,
         fc.fileName AS consultationFileName,
         fc.filePath AS consultationFilePath,
         fr.fileName AS responseFileName,
         fr.filePath AS responseFilePath,
         u.avatar,
-        u.nombre AS patientName,
+        u.firstName AS patientName,
         u.email AS patientEmail,
-        r.rating
+        r.rating,
+        CONCAT(d.firstName, ' ', d.lastName) AS doctorName
     FROM 
         consultations c
     LEFT JOIN 
-        files_consultations fc ON c.id = fc.consultation_id
+        files_consultations fc ON c.id = fc.consultationId
     LEFT JOIN 
-        files_responses fr ON c.id = fr.consultation_id
+        files_responses fr ON c.id = fr.responseId
     LEFT JOIN 
-        users u ON c.patient_id = u.id
+        users u ON c.patientId = u.id
     LEFT JOIN 
-        responses r ON c.id = r.consultation_id
+        responses r ON c.id = r.consultationId
+    LEFT JOIN 
+        users d ON c.doctorId = d.id
     WHERE 
         c.specialityId = ?`,
-    [specialityId]
-  );
-  return consultations;
+		[specialityId]
+	);
+	return consultations;
 };
