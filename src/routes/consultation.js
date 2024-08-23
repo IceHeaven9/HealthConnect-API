f6b71561812b4ba1131b9187cf52c565a8ab38ee
Ivan Sanchez <mourazos1995@gmail.com>
2024-08-23 17:30:29 +0200

    change-names

--- src/routes/consultation.js
+++ src/routes/consultation.js
@@ -1,29 +1,29 @@
-import { Router } from "express";
-import { createConsultationController } from "../controllers/consultationControllers/createConsultation.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { getConsultationDetailsController } from "../controllers/consultationControllers/getconsultationbyid.js";
-import { getConsultationController } from "../controllers/consultationControllers/getConsultations.js";
-import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { Router } from 'express';
+import { createConsultationController } from '../controllers/consultation/createConsultation.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
+import { getConsultationDetailsController } from '../controllers/consultation/getconsultationbyid.js';
+import { getConsultationController } from '../controllers/consultation/getConsultations.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 export const consultationRoutes = Router();
 
 // Ruta para crear una consulta
 consultationRoutes.post(
-  "/consultations",
-  authMiddleware,
-  asyncHandler(createConsultationController)
+	'/consultations',
+	authMiddleware,
+	asyncHandler(createConsultationController)
 );
 
 // Ruta para obtener todas las consultas
 consultationRoutes.get(
-  "/consultations",
-  authMiddleware,
-  asyncHandler(getConsultationController)
+	'/consultations',
+	authMiddleware,
+	asyncHandler(getConsultationController)
 );
 
 // Ruta para obtener los datos de una consulta
 consultationRoutes.get(
-  "/consultations/:id/details",
-  authMiddleware,
-  asyncHandler(getConsultationDetailsController)
+	'/consultations/:id/details',
+	authMiddleware,
+	asyncHandler(getConsultationDetailsController)
 );
