2845e7f90af577c92767c6fcf9b7b4c388b05966
Ivan Sanchez <mourazos1995@gmail.com>
2024-09-04 20:24:51 +0200

    fix

--- src/database/availability.js
+++ src/database/availability.js
@@ -1,20 +1,21 @@
-import {Db} from './structure/db.js';
+import { Db } from './structure/db.js';
 export const availabilityDoctors = async (specialityId, doctorId = null) => {
-  let query = 'SELECT u.id, u.firstName, u.lastName FROM users u JOIN user_specialities us ON u.id = us.userId WHERE us.specialityId = ? AND u.userType = "doctor"';
-  const params = [specialityId];
+	let query =
+		'SELECT u.id, u.firstName, u.lastName, u.avatar FROM users u JOIN user_specialities us ON u.id = us.userId WHERE us.specialityId = ? AND u.userType = "doctor"';
+	const params = [specialityId];
+	if (doctorId) {
+		query += ' AND u.id = ?';
+		params.push(doctorId);
+	}
 
-  if (doctorId) {
-    query += ' AND u.id = ?';
-    params.push(doctorId);
-  }
-
-  const [rows] = await Db.query(query, params);
-  return rows;
+	const [rows] = await Db.query(query, params);
+	return rows;
 };
-export const availabilityconsultations = async (specialityId, date) => {
-  const [rows] = await Db.query(
-    'SELECT doctorId, date FROM consultations WHERE specialityId = ? AND DATE(date) = ?',
-    [specialityId, date]
-  )
-  return rows;
-}
+
+export const availabilityConsultations = async (specialityId, date) => {
+	const [rows] = await Db.query(
+		'SELECT doctorId, date FROM consultations WHERE specialityId = ? AND DATE(date) = ?',
+		[specialityId, date]
+	);
+	return rows;
+};
