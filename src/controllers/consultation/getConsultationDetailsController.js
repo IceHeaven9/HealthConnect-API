2ea362466262dcd296bdc14838a4388f3d686e68
Ivan Sanchez <mourazos1995@gmail.com>
2024-09-07 20:36:44 +0200

    fixed-error

--- src/controllers/consultation/getConsultationDetailsController.js
+++ src/controllers/consultation/getConsultationDetailsController.js
@@ -4,24 +4,24 @@ import {
 	getConsultationsDetailsByDoctorId,
 	getResponseFilesByResponseId,
 } from '../../database/consultation.js';
-import { findDoctorById } from '../../database/users.js';
+import { findUserById } from '../../database/users.js';
 
 // Controlador para obtener los detalles de una consulta
 
 export const getConsultationDetailsController = async (req, res) => {
 	const userId = req.currentUser.id;
-	const user = await findDoctorById(userId);
+	const user = await findUserById(userId);
 	const { id } = req.params;
+	const consultationFiles = await getConsultationFilesByConsultationId(id);
 
 	if (user.userType === 'patient') {
 		const consultation = await getConsultationDetailsByPatientId(req, res);
-		res.status(200).json(consultation, consultationFiles);
+		res.status(200).json({ consultation, consultationFiles });
 	}
 
 	if (user.userType === 'doctor') {
 		const consultation = await getConsultationsDetailsByDoctorId(id);
 		const responseId = consultation.responseId;
-		const consultationFiles = await getConsultationFilesByConsultationId(id);
 		const responseFiles = await getResponseFilesByResponseId(responseId);
 
 		const result = {
