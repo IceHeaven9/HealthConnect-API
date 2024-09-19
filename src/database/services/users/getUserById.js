2e3d0ff45d66787edff412b42d32c49cfb56a0ac
Cristhian Medrano <cristhian_devfs@outlook.com>
2024-09-19 12:38:33 +0200

    changes

--- src/database/services/users/getUserById.js
+++ src/database/services/users/getUserById.js
@@ -2,9 +2,9 @@ import { Db } from "../../structure/db.js"
 
 export const getUserById = async (id) => {
 
-const [user] = await Db.query(
-  "SELECT * FROM users WHERE id =?",
-  [id]
-)
+  const [user] = await Db.query(
+    "SELECT CONCAT(firstName, ' ', lastName) AS fullName, email, biography, experience FROM users WHERE id = ?",
+    [id]
+  )
 return user;
 }
