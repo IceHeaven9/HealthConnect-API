de46b7738911430e4d53a233f28641ce1152dacf
Ivan Sanchez <mourazos1995@gmail.com>
2024-09-25 19:42:44 +0200

    changes-query-user

--- src/database/services/users/getUserById.js
+++ src/database/services/users/getUserById.js
@@ -1,10 +1,9 @@
-import { Db } from "../../structure/db.js"
+import { Db } from '../../structure/db.js';
 
 export const getUserById = async (id) => {
-
-  const [user] = await Db.query(
-    "SELECT firstName, lastName, userName, email, biography, experience FROM users WHERE id = ?",
-    [id]
-  )
-return user;
-}
+	const [user] = await Db.query(
+		'SELECT firstName, lastName, userName, email, biography, experience, avatar FROM users WHERE id = ?',
+		[id]
+	);
+	return user;
+};
